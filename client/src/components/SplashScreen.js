63ff1d9df77675a1b57a86adf98cf7f4903b19d3
Aaron <aaron.lin.2@stonybrook.edu>
2022-11-21 15:57:23 -0500

    splash screen and background

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,23 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom'
+
 export default function SplashScreen() {
     return (
-        <div id="splash-screen">
+        <Box id="splash-screen">
             Playlister
-        </div>
-    )
+            <Grid container justifyContent="center" spacing={4} padding={10}>
+                <Grid item>
+                    <Button size="large" variant="contained"><Link to='/login/'>Login</Link></Button>
+                </Grid>
+                <Grid item>
+                    <Button size="large" variant="contained"><Link to='/register/'>Create Account</Link></Button>
+                </Grid>
+                <Grid item>
+                    <Button size="large" variant="contained"><Link>Continue as Guest</Link></Button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
